0799a71c50765da96e72127720a75310191b4601
linwj <linwj@chinaums.com>
2025-02-27 17:14:19 +0800

    feat: update page

---
+++ assets/About.C59CmcX0.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_plugin-vue_export-helper.DYAABpgn.js";import{c as e,o}from"./index.Ce16KfDP.js";const t=r({},[["render",function(r,t){return o(),e("div",null," about ")}]]);export{t as default};
---
+++ assets/News.DV5CBEde.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper.DYAABpgn.js";import{c as r,o as n}from"./index.Ce16KfDP.js";const o=e({},[["render",function(e,o){return n(),r("div",null," news ")}]]);export{o as default};
