d18d928ad09760e86260d1f537724a24b9d2b397
duopin-tech <lwj328043646@gmail.com>
2025-04-05 08:34:25 +0800

    update dist

--- assets/Dashboard-BnT-U8hI.js
+++
@@ -1,1 +0,0 @@
-import{_ as r,c as a,o as n}from"./index-jadI9PiH.js";const o=r({},[["render",function(r,o){return n(),a("div",null,"dashboard")}]]);export{o as default};
---
+++ assets/Optimized-CKt5Ic8A.js
@@ -0,0 +1,1 @@
+import{_ as e,c as n,o}from"./index-CAb7rCbF.js";const r=e({},[["render",function(e,r){return o(),n("div",null,"optimized")}]]);export{r as default};
